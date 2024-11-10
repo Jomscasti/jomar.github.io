7c9bacee396faa256d536ff1d03ae66ffef6ba2d
Jomscasti <jomaricastillo8@gmail.com>
2024-11-10 21:18:07 +0800

    Update icon on index.php

--- M01/index.php
+++ M01/index.php
@@ -33,6 +33,7 @@ $result = executeQuery($query);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/6049/6049685.png">
 </head>
 
 <body>
