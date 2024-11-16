69f74ee1e40b7f51f7923fc7caa2bd3e9dcfc802
Jomscasti <jomaricastillo8@gmail.com>
2024-11-16 21:45:15 +0800

    Add files via upload

---
+++ A06/assets/footer.php
@@ -0,0 +1,42 @@
+<style>
+
+.footerText {
+    font-size: 1.8rem;
+    font-weight: bold;
+    color: #f2b2ff;
+    text-shadow: 2px 2px 8px #ff00ff, -2px -2px 8px #00f0ff;
+}
+
+.footerSubtext {
+  color: #a4e4ff;
+  font-size: 1rem;
+  margin-top: 10px;
+  text-shadow: 1px 1px 4px #00f0ff;
+}
+
+.socialLink {
+  color: #f2b2ff;
+  font-size: 1rem;
+  text-decoration: none;
+  margin: 0 10px;
+  transition: color 0.3s;
+}
+
+.socialLink:hover {
+  color: #ff8cff;
+  text-shadow: 1px 1px 6px #ff00ff;
+}
+
+</style>
+
+<footer class="footer mt-5 py-4 text-center" id="footer">
+  <div class="container-fluid overflow-x: hidden">
+    <p class="footerText display-6 mb-1">Parrhesia 2024 - A world of free expression</p>
+    <hr class="my-4">
+    <p class="footerSubtext">Follow me on:
+      <a href="https://www.facebook.com/rafael.gaus.16" class="socialLink">Facebook</a>
+      <a href="https://x.com/JomCasti" class="socialLink">Twitter</a>
+      <a href="https://www.instagram.com/jomsheeesh/" class="socialLink">Instagram</a>
+    </p>
+  </div>
+</footer>
